1ee872b864bda026d0d1cc1c73104f2fd989b352
alexisdabest <alex.2@live.com.au>
2013-05-25 17:22:12 +1000

    here we go

---
+++ common/mT/alex/moretools/MoreTools.java
@@ -0,0 +1,39 @@
+package mT.alex.moretools;
+
+import mT.alex.moretools.lib.reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod( modid = reference.MOD_ID ,  name = reference.MOD_NAME , version = reference.VERSION )
+
+public class MoreTools {
+	
+	@PreInit
+	 public void preInit(FMLPreInitializationEvent event) { 
+		
+		
+	}
+	
+	@Init
+	public void init(FMLInitializationEvent event) {
+		
+		
+	}
+	
+	@PostInit 
+	 public void postInit (FMLPostInitializationEvent event){
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ common/mT/alex/moretools/lib/reference.java
@@ -0,0 +1,9 @@
+package mT.alex.moretools.lib;
+
+public class reference {
+	
+	
+	public static final String MOD_ID = "motools ";
+	public static final String MOD_NAME = "more tools";
+	public static final String VERSION = "0.0.1";
+}
